2d3413011429142686e0822afe4067fb68c185c1
g0ooo0gle <mail.keisuke.nakamura01234@gmail.com>
2025-08-30 18:54:16 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/github/g0ooo0gle/chronofeednewsapi/ChronofeednewsapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.g0ooo0gle.chronofeednewsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronofeednewsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronofeednewsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/g0ooo0gle/chronofeednewsapi/ChronofeednewsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.g0ooo0gle.chronofeednewsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronofeednewsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
